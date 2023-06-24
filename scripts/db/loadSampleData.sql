4e3e55cdc46167a5dbea335082fa5e1e3abb1a67
shriya2603 <sundriyalshriya@gmail.com>
2023-06-24 23:09:45 +0530

    added preload sample data sql script

---
+++ scripts/db/loadSampleData.sql
@@ -0,0 +1,20 @@
+
+INSERT INTO customers (customer_name, address, phone) VALUES ('Shriya','India', '12345678');
+INSERT INTO customers (customer_name, address, phone) VALUES ('Shreya','India', '12378098');
+INSERT INTO customers (customer_name, address, phone) VALUES ('Ram','India', '80988098');
+
+
+INSERT INTO products (product_name, ean_barcode, category, sub_category, description, created_at, updated_at)
+VALUES
+  ('Samsung 4K Smart TV', '9876543210987', 'Electronics', 'Televisions', 'High-quality smart TV with 4K resolution.', NOW(), NOW()),
+  ('Nike Air Max Sneakers', '4567891234567', 'Fashion', 'Shoes', 'Stylish sneakers with comfortable cushioning.', NOW(), NOW()),
+  ('Canon EOS R6 Camera', '7890123456789', 'Electronics', 'Cameras', 'Professional-grade mirrorless camera for photography enthusiasts.', NOW(), NOW());
+
+
+INSERT INTO products (product_name, ean_barcode, category, sub_category, description, created_at, updated_at)
+VALUES
+  ('Apple MacBook Pro', '2222222222222', 'Electronics', 'Laptops', 'Powerful laptop for professionals and creatives.', NOW(), NOW()),
+  ('Samsung Galaxy Watch', '7777777777777', 'Electronics', 'Wearable Devices', 'Smartwatch with fitness tracking and advanced features.', NOW(), NOW()),
+  ('Adidas Ultraboost Shoes', '3333333333333', 'Fashion', 'Shoes', 'Comfortable running shoes with responsive cushioning.', NOW(), NOW());
+
+
