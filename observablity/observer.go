b5333e28f53877fcbb06fd4b7e37eb3214b3f70a
shriya2603 <sundriyalshriya@gmail.com>
2023-07-02 18:12:05 +0530

    added up time metric

--- observablity/observer.go
+++ observablity/observer.go
@@ -3,10 +3,28 @@ package observablity
 import (
 	"sync"
 	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	upTime = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "uptime",
+		Help: "Uptime of service"},
+		[]string{"service_name"})
 )
 
 func init() {
+	prometheus.MustRegister(upTime)
+}
+func prometheusHandler() gin.HandlerFunc {
+	h := promhttp.Handler()
 
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
 }
 
 func StartObserver(wg *sync.WaitGroup, observerAddr string, serverName string, bootTime float64) {
@@ -18,9 +36,13 @@ func StartObserver(wg *sync.WaitGroup, observerAddr string, serverName string, b
 			select {
 			case <-time.After(30 * time.Second):
 				bootTime += 30.0
+				upTime.WithLabelValues(serverName).Set(bootTime)
 			}
 
 		}
 	}(serverName)
-
+	upTime.WithLabelValues(serverName).Set(bootTime)
+	r := gin.New()
+	r.GET("/metrics", prometheusHandler())
+	r.Run(observerAddr)
 }
